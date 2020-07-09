bc5026ad4e83dbed76d0b7883bbb78ce3bce52e5
Mark Duenas <markgdlduenas@gmail.com>
2020-07-09 12:57:05 -0700

    added buttons
    birthday buttons that increment birthday by 1 if clicked.

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 
 class PersonCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            age : this.props.age
+        }
+    }
+
+    celebrateBirthday = () => {
+        this.setState((state) => ({
+            age: state.age + 1
+        }));
+    }
+    
     render() {
-        const { firstName, lastName, age, hairColor} = this.props;
+        const { firstName, lastName, hairColor} = this.props;
         return (
             <div>
                 <h1> { lastName }, { firstName }  </h1>
-                <p> Age : { age } </p>
+                <p> Age : { this.state.age } </p>
                 <p> Hair Color: { hairColor }</p>
+                <button onClick = { this.celebrateBirthday }> Celebrate {firstName}'s' Birthday!</button>
             </div>
 
         );
