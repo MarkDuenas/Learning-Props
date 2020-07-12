c751197274ab79129dd526c1acd96b475442daf7
Mark Duenas <markgdlduenas@gmail.com>
2020-07-12 15:16:02 -0700

    Changed calss component to functional

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,31 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react'
 
-class PersonCard extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            age : this.props.age
-        }
-    }
-
-    celebrateBirthday = () => {
-        this.setState((state) => ({
-            age: state.age + 1
-        }));
-    }
-    
-    render() {
-        const { firstName, lastName, hairColor} = this.props;
-        return (
-            <div>
-                <h1> { lastName }, { firstName }  </h1>
-                <p> Age : { this.state.age } </p>
-                <p> Hair Color: { hairColor }</p>
-                <button onClick = { this.celebrateBirthday }> Celebrate {firstName}'s' Birthday!</button>
-            </div>
-
-        );
-    }
+const PersonCard = (props) => {
+    return (
+        <div>
+            <h1> { props.lastName }, { props.firstName } </h1>
+            <p> Age: {props.age} </p>
+            <p> Hair Color: {props.hairColor}</p>
+        </div>
+    )
 }
 
-export default PersonCard;
+export default PersonCard
